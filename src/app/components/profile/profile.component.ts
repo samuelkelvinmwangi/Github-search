056e0b8aaab42200a9ba881017e310b95527e1ca
Samuel Mwangi <samuelkelvinmwangi@gmail.com>
2021-08-29 21:44:05 +0300

    adding profice service and using in constructor

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileService } from 'src/app/services/profile.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ProfileService:ProfileService) {
+    this.ProfileService.getProfileInfo().subscribe((profile: any) => {
+      console.log(profile)
+    });
+  }
 
   ngOnInit(): void {
   }
